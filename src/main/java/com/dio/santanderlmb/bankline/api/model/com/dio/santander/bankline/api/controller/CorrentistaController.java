e3524ea6214bda93908af3b91a6d266b66dbb64d
lmbernardo7520112 <lmbernardo752011@gmail.com>
2022-05-07 18:26:09 -0300

    CRUD 1:19:35

--- src/main/java/com/dio/santanderlmb/bankline/api/model/com/dio/santander/bankline/api/controller/CorrentistaController.java
+++ src/main/java/com/dio/santanderlmb/bankline/api/model/com/dio/santander/bankline/api/controller/CorrentistaController.java
@@ -1,8 +1,10 @@
 package com.dio.santanderlmb.bankline.api.model.com.dio.santander.bankline.api.controller;
 
 
+import com.dio.santanderlmb.bankline.api.model.com.dio.santander.bankline.api.dto.NovoCorrentista;
 import com.dio.santanderlmb.bankline.api.model.com.dio.santander.bankline.api.model.Correntista;
 import com.dio.santanderlmb.bankline.api.model.com.dio.santander.bankline.api.repository.CorrentistaRepository;
+import com.dio.santanderlmb.bankline.api.model.com.dio.santander.bankline.api.service.CorrentistaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,13 +17,17 @@ public class CorrentistaController {
     @Autowired
     private CorrentistaRepository repository;
 
+    @Autowired
+    private CorrentistaService service;
+
     @GetMapping
     public List<Correntista> findAll(){
         return repository.findAll();
     }
 
     @PostMapping
-    public  void save(@RequestBody Correntista correntista) {
+    public void save(@RequestBody NovoCorrentista correntista) {
+        service.save(correntista);
 
     }
 }
--- src/main/java/com/dio/santanderlmb/bankline/api/model/com/dio/santander/bankline/api/service/CorrentistaService.java
+++ src/main/java/com/dio/santanderlmb/bankline/api/model/com/dio/santander/bankline/api/service/CorrentistaService.java
@@ -3,10 +3,16 @@ package com.dio.santanderlmb.bankline.api.model.com.dio.santander.bankline.api.s
 import com.dio.santanderlmb.bankline.api.model.com.dio.santander.bankline.api.dto.NovoCorrentista;
 import com.dio.santanderlmb.bankline.api.model.com.dio.santander.bankline.api.model.Conta;
 import com.dio.santanderlmb.bankline.api.model.com.dio.santander.bankline.api.model.Correntista;
+import com.dio.santanderlmb.bankline.api.model.com.dio.santander.bankline.api.repository.CorrentistaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class CorrentistaService {
+    @Autowired
+    private CorrentistaRepository repository;
     public void  save(NovoCorrentista novoCorrentista) {
         Correntista correntista = new Correntista();
         correntista.setCpf(novoCorrentista.getCpf());
@@ -14,7 +20,9 @@ public class CorrentistaService {
 
         Conta conta = new Conta();
         conta.setSaldo(0.0);
+        conta.setNumero((new Date().getTime()));
 
-        correntista.setConta(null);
+        correntista.setConta(conta);
+        repository.save(correntista);
     }
 }
