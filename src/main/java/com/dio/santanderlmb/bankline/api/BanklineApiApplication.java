5008042c3dc238117644e24d216dc79d706ad5e3
lmbernardo7520112 <lmbernardo752011@gmail.com>
2022-05-07 13:58:04 -0300

    Hibernate fixed 47:26

--- src/main/java/com/dio/santanderlmb/bankline/api/BanklineApiApplication.java
+++ src/main/java/com/dio/santanderlmb/bankline/api/BanklineApiApplication.java
@@ -1,8 +1,17 @@
 package com.dio.santanderlmb.bankline.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
+
+@Configuration
+@EnableAutoConfiguration
+@Component
+@ComponentScan
 @SpringBootApplication
 public class BanklineApiApplication {
 
